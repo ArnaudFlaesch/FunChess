9970854ee204442c1970e49caf4e5c6802e2f0c3
Arnaud Flaesch <arnaudflaesch@hotmail.com>
2017-04-14 21:53:55 +0200

    Chess game
    The game fetches the player's color at start and the list of pawns. When a player moves a piece, the data (piece and destination coordinates) is sent to the server using Elm's websockets. Data received by the sockets (the current player color and the list of remaining pawns) is decoded using Json.decode.

---
+++ Main.elm
@@ -0,0 +1,42 @@
+module Main exposing (..)
+
+import Html exposing(..)
+import Json.Decode exposing (decodeString)
+import WebSocket
+
+import Grid exposing(renderGrid)
+import Model exposing(..)
+import Server exposing (..)
+
+init : (Model, Cmd Msg)
+init =
+  (Model [] Nothing (Select 0 0) "" "" "", initBoard)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  WebSocket.listen serverUrl NextTurn
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Select coordsX coordsY ->
+            (Model model.pawnsList (searchIfPieceIsOnTile model.pawnsList coordsX coordsY) (Move coordsX coordsY) model.currentPlayer model.playerColor "", Cmd.none)
+        Move coordsX coordsY ->
+            case model.selectedPawn of
+                Just pawn -> (Model model.pawnsList Nothing (Select coordsX coordsY) model.currentPlayer model.playerColor "", WebSocket.send serverUrl (encodeServerRequest(ServerRequest pawn coordsX coordsY)))
+                Nothing -> (model, Cmd.none)
+        NextTurn gameStateJSON -> case (decodeString decodeGameState gameStateJSON) of
+                                                (Ok gameState) -> (Model gameState.pawnsListFromServer Nothing (Select 0 0) gameState.playerColor model.playerColor gameState.winner, Cmd.none)
+                                                (Err _) -> (model, Cmd.none)
+        GetGameState (Ok gameState) -> (Model gameState.pawnsListFromServer Nothing (Select 0 0) gameState.currentPlayer gameState.playerColor gameState.winner, Cmd.none)
+        GetGameState (Err _) -> (model, Cmd.none)
+        None -> (model, Cmd.none)
+
+main: Program Never Model Msg
+main =
+    Html.program
+      { init = init
+      , view = renderGrid
+      , update = update
+      , subscriptions = subscriptions
+      }
