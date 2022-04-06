6690d6cbc7baf7bd3869c3250c5771eec9d9edc1
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-04-06 21:20:52 +0200

    Update dependencies and add CI

--- test/serverTests.js
+++ test/serverTests.js
@@ -1,24 +1,24 @@
 var server = require('../server'),
-    assert = require('assert'),
-    http = require('http');
+  assert = require('assert'),
+  http = require('http');
 
 var port = process.env.PORT || 1337;
 
 describe('server', function () {
-    before(function () {
-        server.listen(port)
-    });
+  before(function () {
+    server.listen(port);
+  });
 
-    after(function () {
-        server.close();
-    });
+  after(function () {
+    server.close();
+  });
 });
 
 describe('Server status and Message', function () {
-    it('status response should be equal 200', function (done) {
-        http.get('http://localhost:'+port+'/start', function (response) {
-            assert.equal(response.statusCode, 200);
-            done();
-        });
+  it('status response should be equal 200', function (done) {
+    http.get('http://localhost:' + port + '/start', function (response) {
+      assert.equal(response.statusCode, 200);
+      done();
     });
+  });
 });
